0c1dace1ab4ee723de9e892bd0c9ed2bc2e8a3bb
kold1kz <91202952+kold1kz@users.noreply.github.com>
2023-11-01 14:33:53 +0300

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Пример ожидания доступности базы данных PostgreSQL
-alembic revision --autogenerate -m "Migration message"
-
-alembic upgrade head
-
-gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
-
